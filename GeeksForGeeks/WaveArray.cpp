4114290efc08060bb0501ac95fe50fa974ec77cc
divyjain62000 <divyjain62000@gmail.com>
2021-10-05 08:36:00 +0530

    First Commit

---
+++ GeeksForGeeks/WaveArray.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+void convertToWave(vector<int>& arr, int n)
+{
+for(int i=0;i<n-1;i=i+2) {
+int g=arr[i];
+arr[i]=arr[i+1];
+arr[i+1]=g;
+}
+}
+};
+
+int main()
+{
+int t,n;
+cin>>t;
+while(t--)
+{
+cin>>n;
+vector<int> a(n);
+for(int i=0;i<n;i++)
+cin>>a[i];
+Solution ob;
+ob.convertToWave(a, n);
+for(int i=0;i<n;i++)
+cout<<a[i]<<" ";
+cout<<endl;
+}
+}
